4a1f8140b44b061282110d7110dc203e0d23d9f1
Sarahi Villegas <sg.villegasq@gmail.com>
2021-05-05 20:01:11 +0100

    video 15: implementing play/pause functionality

--- spotify/util.py
+++ spotify/util.py
@@ -59,7 +59,7 @@ def refresh_spotify_token(session_key):
     acces_token = response.get('access_token')
     token_type = response.get('token_type')
     expires_in = response.get('expires_in')
-    refresh_token = response.get('refresh_token')
+    # refresh_token = response.get('refresh_token')
 
     update_or_create_user_tokens(
         session_key, acces_token, token_type, expires_in, refresh_token)
@@ -81,3 +81,11 @@ def execute_spotify_api_request(session_key, endpoint, post_=False, put_=False):
         return response.json()
     except:
         return {'Error': 'Issue with request'}
+
+
+def pause_song(session_key):
+    return execute_spotify_api_request(session_key, 'player/pause', put_=True)
+
+
+def play_song(session_key):
+    return execute_spotify_api_request(session_key, 'player/play', put_=True)
